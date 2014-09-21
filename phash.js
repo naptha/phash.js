7067a0c10c1f2a46e3c6074de62b003e3a479456
Kevin Kwok <antimatter15@gmail.com>
2014-09-21 15:47:10 -0400

    phash

--- phash.js
+++ phash.js
@@ -1,25 +1,53 @@
-// alyson hannigan
-// 0110000000001100000100100000000000100000000000000 original
-// 0110000000001100000100100001000000000000000000000 green
-// 0111100000000110000110110000000000010000010000000 cropped
-// 0111110000000110000110100000000000010000010000000 hashtag yolo
-
-// bill murray
-// 0010001001011001010111001001101101111010100000100 original
-// 0110001001010001010000100001101000001000100100100 meme
-
-
 // https://ironchef-team21.googlecode.com/git-history/75856e07bb89645d0e56820d6e79f8219a06bfb7/ironchef_team21/src/ImagePHash.java
 
 function pHash(img){
 	var size = 32,
 		smallerSize = 8;
 
+	var canvas = document.createElement('canvas'),
+		ctx = canvas.getContext('2d');
+	
+	document.body.appendChild(canvas)
+
+	/* 1. Reduce size. 
+	 * Like Average Hash, pHash starts with a small image. 
+	 * However, the image is larger than 8x8; 32x32 is a good size. 
+	 * This is really done to simplify the DCT computation and not 
+	 * because it is needed to reduce the high frequencies.
+	 */
+
+	canvas.width = size;
+	canvas.height = size;
+	// ctx.drawImage(img, 0, 0, size, size);
+	ctx.drawImage(img, 0, -size, size, size * 3);
+	var im = ctx.getImageData(0, 0, size, size);
+
+	/* 2. Reduce color. 
+	 * The image is reduced to a grayscale just to further simplify 
+	 * the number of computations.
+	 */
+
+	var vals = new Float64Array(size * size);
+	for(var i = 0; i < size; i++){
+		for(var j = 0; j < size; j++){
+			var base = 4 * (size * i + j);
+			vals[size * i + j] = 0.299 * im.data[base] + 
+				0.587 * im.data[base + 1] + 
+				0.114 * im.data[base + 2];
+		}
+	}
+
+	/* 3. Compute the DCT. 
+	 * The DCT separates the image into a collection of frequencies 
+	 * and scalars. While JPEG uses an 8x8 DCT, this algorithm uses 
+	 * a 32x32 DCT.
+	 */
+
 	function applyDCT2(N, f){
 		// initialize coefficients
 		var c = new Float64Array(N);
-		for(var i = 0; i < N; i++) c[i] = 1;
-		c[0] = 1/Math.sqrt(2);
+		for(var i = 1; i < N; i++) c[i] = 1;
+		c[0] = 1 / Math.sqrt(2);
 		
 		// output goes here
 		var F = new Float64Array(N * N);
@@ -48,57 +76,24 @@ function pHash(img){
 		return F
 	}
 
-	var canvas = document.createElement('canvas'),
-		ctx = canvas.getContext('2d');
-	
-	document.body.appendChild(canvas)
-
-	canvas.width = size;
-	canvas.height = size;
-	
-	/* 1. Reduce size. 
-	 * Like Average Hash, pHash starts with a small image. 
-	 * However, the image is larger than 8x8; 32x32 is a good size. 
-	 * This is really done to simplify the DCT computation and not 
-	 * because it is needed to reduce the high frequencies.
-	 */
-
-	ctx.drawImage(img, 0, 0, size, size);
-	var im = ctx.getImageData(0, 0, size, size);
-
-	/* 2. Reduce color. 
-	 * The image is reduced to a grayscale just to further simplify 
-	 * the number of computations.
-	 */
-
-	var vals = new Float64Array(size * size);
-	for(var i = 0; i < size; i++){
-		for(var j = 0; j < size; j++){
-			var base = 4 * (size * i + j);
-			vals[size * i + j] = 0.299 * im.data[base] + 
-				0.587 * im.data[base + 1] + 
-				0.114 * im.data[base + 2];
-		}
-	}
-
-	/* 3. Compute the DCT. 
-	 * The DCT separates the image into a collection of frequencies 
-	 * and scalars. While JPEG uses an 8x8 DCT, this algorithm uses 
-	 * a 32x32 DCT.
-	 */
-
 	var dctVals = applyDCT2(size, vals);
-
+	
+	// for(var x = 0; x < size; x++){
+	// 	for(var y = 0; y < size; y++){
+	// 		ctx.fillStyle = (dctVals[size * x + y] > 0) ? 'white' : 'black';
+	// 		ctx.fillRect(x, y, 1, 1)
+	// 	}
+	// }
 	/* 4. Reduce the DCT. 
 	 * This is the magic step. While the DCT is 32x32, just keep the 
 	 * top-left 8x8. Those represent the lowest frequencies in the 
 	 * picture.
 	 */
 
-	var total = 0;
-	for(var x = 0; x < smallerSize; x++){
-		for(var y = 0; y < smallerSize; y++){
-			total += dctVals[size * x + y]
+	var vals = []
+	for(var x = 1; x <= smallerSize; x++){
+		for(var y = 1; y <= smallerSize; y++){
+			vals.push(dctVals[size * x + y])
 		}
 	}
 
@@ -109,7 +104,9 @@ function pHash(img){
 	 * the other values and will throw off the average).
 	 */
 
-	var avg = total / (smallerSize * smallerSize - 1);
+	var median = vals.slice(0).sort(function(a, b){
+		return a - b
+	})[Math.floor(vals.length / 2)];
 
 	/* 6. Further reduce the DCT. 
 	 * This is the magic step. Set the 64 hash bits to 0 or 1 
@@ -122,14 +119,9 @@ function pHash(img){
 	 * adjustments without a problem.
 	 */
 
-	var hash = '';
-	for(var x = 1; x < smallerSize; x++){
-		for(var y = 1; y < smallerSize; y++){
-			hash += dctVals[size * x + y] > avg ? '1' : '0';
-		}
-	}
-
-	return hash;
+	return vals.map(function(e){
+		return e > median ? '1' : '0';
+	}).join('');
 }
 
 
